de118482fd6bbe5700e4c0d85033cdad72ed921a
qqqq4u <danila16102003@rambler.ru>
2021-12-14 00:11:36 +0300

    fixes

--- code/functions.cpp
+++ code/functions.cpp
@@ -74,22 +74,38 @@ void PrintMatrix(double **matrix, int n, int m, bool isScientific, int precision
     int count = 0;
     if (isScientific)
     {
-        count = w.ws_col / (precision + 8);
-        for (int z = 0; z < m / count; z++)
+        int z = 0;
+        int q = 0;
+        count = w.ws_col / (precision + 9);
+        for (z = 0; z < m / count; z++)
         {
             for (size_t i = 0; i < n; i++)
             {
                 for (size_t j = count * z; j < count * (z + 1); j++)
                 {
                     std::cout << std::scientific << std::setprecision(precision) << std::setw(precision + 9) << matrix[i][j];
+                    q = j;
                 }
                 std::cout << std::endl;
             }
             std::cout << std::endl;
         }
+        if (q + 1 < m)
+        {
+            for (size_t i = 0; i < n; i++)
+            {
+                for (size_t j = q + 1; j < m; j++)
+                {
+                    std::cout << std::scientific << std::setprecision(precision) << std::setw(precision + 9) << matrix[i][j];
+                }
+                std::cout << std::endl;
+            }
+        }
     }
     else
     {
+        int z = 0;
+        int q = 0;
         count = w.ws_col / (precision + 4);
         for (int z = 0; z < m / count; z++)
         {
@@ -98,10 +114,22 @@ void PrintMatrix(double **matrix, int n, int m, bool isScientific, int precision
                 for (size_t j = count * z; j < count * (z + 1); j++)
                 {
                     std::cout << std::fixed << std::setprecision(precision) << std::setw(precision + 4) << matrix[i][j];
+                    q = j;
                 }
                 std::cout << std::endl;
             }
             std::cout << std::endl;
+        }
+        if (q + 1 < m)
+        {
+            for (size_t i = 0; i < n; i++)
+            {
+                for (size_t j = q + 1; j < m; j++)
+                {
+                    std::cout << std::fixed << std::setprecision(precision) << std::setw(precision + 4) << matrix[i][j];
+                }
+                std::cout << std::endl;
+            }
         }
     }
 }
--- code/main.cpp
+++ code/main.cpp
@@ -13,21 +13,22 @@ int main()
     double **matrix = CreateMatrix(n);
 
     PrintMatrix(matrix, n, n, isScientific, precision);
+
     DestructMatrix(matrix, n);
 
-    int B[10][10];
-    for (size_t i = 0; i < 10; i++)
-    {
-        for (size_t j = 0; j < 10; j++)
-        {
-            B[i][j] = i * 10 + j;
-        }
-    }
-    PrintMatrix(B);
+    // int B[10][10];
+    // for (size_t i = 0; i < 10; i++)
+    // {
+    //     for (size_t j = 0; j < 10; j++)
+    //     {
+    //         B[i][j] = i * 10 + j;
+    //     }
+    // }
+    // PrintMatrix(B);
 
-    std::cout << B << "  " << B[0] << "  " << B[2] << std::endl;
-    std::cout << B[0][0] << "  " << **B << "  " << *B[0] << std::endl;
-    std::cout << *(*(B + 1)) << "  " << *B[1] << std::endl;
-    std::cout << *(B[0] + 1) << "  " << *(*B + 1) << std::endl;
-    std::cout << B[0][20] << "  " << *(B[0] + 20) << "  " << *B[2] << std::endl;
+    // std::cout << B << "  " << B[0] << "  " << B[2] << std::endl;
+    // std::cout << B[0][0] << "  " << **B << "  " << *B[0] << std::endl;
+    // std::cout << *(*(B + 1)) << "  " << *B[1] << std::endl;
+    // std::cout << *(B[0] + 1) << "  " << *(*B + 1) << std::endl;
+    // std::cout << B[0][20] << "  " << *(B[0] + 20) << "  " << *B[2] << std::endl;
 }
